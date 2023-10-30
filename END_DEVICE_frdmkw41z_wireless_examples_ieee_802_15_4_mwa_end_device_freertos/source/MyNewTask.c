b1256e953fea3b3b06ca099592608efe8f21cc5c
lulucarstensen <ie717781@iteso.mx>
2023-10-30 14:18:19 -0600

    end device follow up, not done yet

---
+++ END_DEVICE_frdmkw41z_wireless_examples_ieee_802_15_4_mwa_end_device_freertos/source/MyNewTask.c
@@ -0,0 +1,180 @@
+/*
+ * MyNewTask.c
+ *
+ *  Created on: Oct 19, 2023
+ *      Author: carstensen
+ */
+
+
+#include "MyNewTask.h"
+
+/* Variables and Prototypes */
+osaEventId_t mMyEvents;
+/* Global Variable to store our TimerID */ tmrTimerID_t myTimerID = gTmrInvalidTimerID_c;
+tmrTimerID_t ledTimerID = gTmrInvalidTimerID_c;
+   /* Handler ID for task */
+osaTaskId_t gMyTaskHandler_ID;
+/* Local variable to store the current state of the LEDs */
+static uint8_t ledsState = 0;
+
+
+
+/* OSA Task Definition */
+OSA_TASK_DEFINE(My_Task, gMyTaskPriority_c, 1, gMyTaskStackSize_c, FALSE );
+
+
+/* Function to init the task */
+void MyTask_Init(void)
+{
+	mMyEvents = OSA_EventCreate(TRUE);
+    /* allocate a timer for use in LEDs */
+	ledTimerID = TMR_AllocateTimer();
+
+	/* The instance of the MAC is passed at task creaton */
+	gMyTaskHandler_ID = OSA_TaskCreate(OSA_TASK(My_Task), NULL);
+}
+
+/* Main custom task */
+void My_Task(osaTaskParam_t argument)
+{
+   osaEventFlags_t customEvent;
+   myTimerID = TMR_AllocateTimer();
+   MyTask_Init(); /* INIT MY NEW TASK */
+
+	while(1)
+	{
+	OSA_EventWait(mMyEvents, osaEventFlagsAll_c, FALSE, osaWaitForever_c, &customEvent);
+
+		if( !gUseRtos_c && !customEvent)
+		{
+			break;
+		}
+          /* Depending on the received event */
+		switch(customEvent){
+
+		case gMyNewTaskEvent1_c:
+			TMR_StartIntervalTimer(myTimerID, /*myTimerID*/
+									1000, /* Timer's Timeout */
+									myTaskTimerCallback, /* pointer to myTaskTimerCallback function */
+									NULL
+			);
+		TurnOffLeds(); /* Ensure all LEDs are turned off */
+
+		break;
+
+		case gMyNewTaskEvent2_c: /* Event called from myTaskTimerCallback */
+
+			if(!ledsState) {
+                   TurnOnLeds();
+                   ledsState = 1;
+			}
+
+			else {
+					TurnOffLeds();
+					ledsState = 0;
+			}
+			break;
+
+		case gMyNewTaskEvent3_c: /* Event to stop the timer */
+			ledsState = 0;
+			TurnOffLeds();
+			TMR_StopTimer(myTimerID);
+			break;
+
+		default:
+			break;
+			}
+	}
+}
+
+
+
+/* This is the function called by the Timer each time it expires */
+static void myTaskTimerCallback(void *param)
+{
+	OSA_EventSet(mMyEvents, gMyNewTaskEvent2_c);
+}
+
+/* Public function to send an event to stop the timer */
+void MyTaskTimer_Stop(void)
+{
+   OSA_EventSet(mMyEvents, gMyNewTaskEvent3_c);
+}
+
+void MyTaskTimer_Start(void)
+{
+	void MyTask_Init(void);
+}
+
+//Start Timer for LED//
+void LED_StartTimer()
+{
+    /* start the timer */
+    if(!TMR_IsTimerActive(ledTimerID))
+    {
+      //TMR_StartIntervalTimer(ledTimerID, 2000, (pfTmrCallBack_t)RGB_Timeout, (void*)((uint32_t)ledTimerID));
+    }
+}
+
+/************** COORDINATOR FUNCTION ****************
+ * Data transport services between peers
+ * Confirms data was sent
+ * Prints PAN ID, LQI and length
+ * Turns on leds according to message content ******/
+static void newMessage(mcpsToNwkMessage_t * message,uint8_t interfaceId,uint8_t mcPendingPackets)
+{
+	uint8_t count;
+	//message->msgData.dataInd.pMsdu;
+	switch(message->msgType)
+	{
+		/* The MCPS-Data confirm is sent by the MAC to the network
+		or application layer when data has been sent. */
+		case gMcpsDataCnf_c:
+			if(mcPendingPackets)
+				mcPendingPackets--;
+			break;
+
+		case gMcpsDataInd_c:
+
+            Serial_PrintHex(interfaceId,&message->msgData.dataInd.srcPanId, 2, gPrtHexNoFormat_c);
+            Serial_PrintHex(interfaceId,&message->msgData.dataInd.mpduLinkQuality, 1, gPrtHexNoFormat_c);
+            Serial_PrintHex(interfaceId,&message->msgData.dataInd.msduLength, 1, gPrtHexNoFormat_c);
+			break;
+
+		default:
+			break;
+	}
+	switch(count)
+	{
+		case 0:
+			Led_RGB(LED_RGB,LED_MAX_RGB_VALUE_c,0,0);
+			break;
+		case 1:
+			Led_RGB(LED_RGB,0,LED_MAX_RGB_VALUE_c,0);
+			break;
+		case 2:
+			Led_RGB(LED_RGB,0,0,LED_MAX_RGB_VALUE_c);
+			break;
+		case 3:
+			Led_RGB(LED_RGB,0,LED_MAX_RGB_VALUE_c,LED_MAX_RGB_VALUE_c);
+			break;
+		case 4:
+			Led_RGB(LED_RGB,LED_MAX_RGB_VALUE_c,0,LED_MAX_RGB_VALUE_c);
+			break;
+		case 5:
+			Led_RGB(LED_RGB,LED_MAX_RGB_VALUE_c,LED_MAX_RGB_VALUE_c,LED_MAX_RGB_VALUE_c);
+			break;
+		default:
+			break;
+	}
+
+}
+
+
+/************** END DEVICE FUNCTION ****************
+ * Data transport services between peers
+ * Confirms data was sent
+ * Prints PAN ID, LQI and length
+ * Turns on leds according to message content ******/
+
+
---
+++ END_DEVICE_frdmkw41z_wireless_examples_ieee_802_15_4_mwa_end_device_freertos/source/MyNewTask.h
@@ -0,0 +1,52 @@
+/*
+ * MyNewTask.h
+ *
+ *  Created on: Oct 19, 2023
+ *      Author: carstensen
+ */
+
+#ifndef MYNEWTASK_H_
+#define MYNEWTASK_H_
+
+/* Fwk */
+#include "TimersManager.h"
+#include "FunctionLib.h"
+#include "LED.h"
+
+/* KSDK */
+#include "fsl_common.h"
+#include "EmbeddedTypes.h"
+#include "fsl_os_abstraction.h"
+/* Fwk */
+#include "SecLib.h"
+#include "SerialManager.h"
+#include "RNG_Interface.h"
+#include "MemManager.h"
+#include "TimersManager.h"
+#include "FunctionLib.h"
+#include "LED.h"
+
+/* 802.15.4 */
+#include "PhyInterface.h"
+#include "MacInterface.h"
+
+/* KSDK */
+#include "board.h"
+#include "fsl_os_abstraction.h"
+
+/* Define the available Task's Events */
+#define gMyNewTaskEvent1_c (1 << 0)
+#define gMyNewTaskEvent2_c (1 << 1)
+#define gMyNewTaskEvent3_c (1 << 2)
+
+#define gMyTaskPriority_c 3
+#define gMyTaskStackSize_c 400
+
+void MyTaskTimer_Start(void);
+void MyTaskTimer_Stop(void);
+void MyTask_Init(void);
+static void myTaskTimerCallback(void *param);
+void My_Task(osaTaskParam_t argument);
+static void newMessage(mcpsToNwkMessage_t * message,uint8_t interfaceId,uint8_t mcPendingPackets);
+
+#endif /* MYNEWTASK_H_ */
